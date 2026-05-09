334bc2440a781e073d3f8c4e5771c7cfc946889f
Ansh011204 <ansh.kiru@gmail.com>
2026-05-09 11:45:51 +0530

    Update standings.py

--- f1-dashboard/backend/app/routers/standings.py
+++ f1-dashboard/backend/app/routers/standings.py
@@ -7,7 +7,7 @@ router = APIRouter()
 async def get_driver_standings():
     async with httpx.AsyncClient() as client:
         r = await client.get(
-            "https://ergast.com/api/f1/current/driverStandings.json"
+            "https://api.jolpi.ca/ergast/f1/current/driverStandings.json"
         )
         data = r.json()
         standings = data["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]
@@ -17,7 +17,7 @@ async def get_driver_standings():
 async def get_constructor_standings():
     async with httpx.AsyncClient() as client:
         r = await client.get(
-            "https://ergast.com/api/f1/current/constructorStandings.json"
+            "https://api.jolpi.ca/ergast/f1/current/constructorStandings.json"
         )
         data = r.json()
         standings = data["MRData"]["StandingsTable"]["StandingsLists"][0]["ConstructorStandings"]
